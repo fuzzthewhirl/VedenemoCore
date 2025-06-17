30fa7cd224add1890cdf607b39dfaab363d014b3
fuzzthwhirl <fuzzthewhirl@outlook.com>
2025-06-17 16:31:32 +0300

    Build file updates part 1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("io.quarkus") version "3.10.0"
+        kotlin("jvm") version "1.9.10"
+    }
+}
 rootProject.name = "VedenemoCore"
