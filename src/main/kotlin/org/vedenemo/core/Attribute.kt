dd0fdf819fd3ad9b98f88838aa9aeacf64a1aecc
fuzzthwhirl <fuzzthewhirl@outlook.com>
2025-06-24 17:32:24 +0300

    Work-in-progress commit

--- src/main/kotlin/org/vedenemo/core/Attribute.kt
+++ src/main/kotlin/org/vedenemo/core/Attribute.kt
@@ -5,17 +5,15 @@ package org.vedenemo.core
  *
  * @property azName ASCII-compliant name of the attribute, must be unique within the model.
  * @property visName Visual name for the attribute, UTF-8 string. Uniqueness is not enforced.
- * @property activeSince The [Version] since which the attribute is considered active.
- * @property deprecatedSince The [Version] since which the attribute is considered deprecated.
  * @property type The [DataType] representing the type of this attribute.
  */
 data class Attribute(
     val azName: String,
     val visName: String,
-    val activeSince: Version,
-    val deprecatedSince: Version,
+    override val activeSince: Version,
+    override val deprecatedSince: Version,
     val type: DataType
-) {
+) : Versionable() {
     init {
         require(azName.matches(Regex("^[a-zA-Z0-9]+\$"))) {
             "azName must contain only ASCII alphanumeric characters."
---
+++ src/main/kotlin/org/vedenemo/core/Versionable.kt
@@ -0,0 +1,12 @@
+package org.vedenemo.core
+
+/**
+ * Abstract base class representing an element with a lifecycle based on model versions.
+ *
+ * @property activeSince The [Version] since which the element is considered active.
+ * @property deprecatedSince The [Version] since which the element is considered deprecated.
+ */
+abstract class Versionable {
+    abstract val activeSince: Version
+    abstract val deprecatedSince: Version
+}
